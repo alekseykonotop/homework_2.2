d4f1b6eb18d84da378c2682b15b3638a3e0266d0
Aleksey Konotop <alekseykonotop@gmail.com>
2018-04-15 20:25:02 +0300

    Загрузил корректные текстовые файлы для анализа, прописал пошаговый план действий программы анализатора.

--- hw_2.2.py
+++ hw_2.2.py
@@ -1,12 +1,42 @@
 # print('Решение домашнего задания по лекции 2.2 "Работа с кодировками, русскими буквами"')
+# Задача: по каждому файлу новостей вывести список 10 самых частовстречающихся слов
+# длинной более 6 символов.
+# == Алгоритм решения ==
+#    Программа должна
+# 0. Спрашивать у пользователя какой файл стоит проверить.
+# 1. Открыть и декодировать текстовый файл
+# 2. Создать множество из слов в этом файле, которые длиннее 6 символов
+# 3. Для каждого элемента множества подсчитать сколько раз этот эл. встречается
+#    в декодированном тексте и записать значение в словарь где ключ - слово, значение - кол-во слова в тексте
+# 4. Создать массив состоящий из словарей, в котором только 1 пара (слово : кол-во вхождения)
+# 5. Отсортировать массив по значению в словаре, т.е. по кол-ву вхождения ( слева самые частовстречаемые слова)
+# 6. Вывести на экран только первые 10 элементов массива
 
-some_text = 'Hello, мир!'
-print(some_text.encode('koi8-r'))
-print('list koi8-r', list(some_text.encode('koi8-r')))
-print('list cp1251', list(some_text.encode('cp1251')))
-print('list utf8', list(some_text.encode('utf8')))
 
-data = bytes([72, 101, 108, 108, 111, 44, 32, 236, 232, 240, 33])
-print('decode cp1251', data.decode('cp1251'))
 
-# закончил просмотр вебинара на 24 минуте
+
+from chardet.universaldetector import UniversalDetector
+
+# import chardet
+#
+#
+# with open('newscy.txt', 'rb') as f:
+#     data = f.read()
+#     # print(data[:50])
+#     result = chardet.detect(data)
+#     print(result)
+#     s = data.decode(result['encoding'])
+#     print('s', s[:350])
+
+# detector = UniversalDetector()
+#
+# with open('newscy.txt', 'rb') as f:
+#     for line in f:
+#         detector.feed(line)
+#         if detector.done:
+#             print(detector.result)
+#             break
+# detector.close()
+# print(detector.result)
+
+
